d800f1486dbe7d3a4f24af24693b6e9be461e348
s27888 <l0605@pjwstk.edu.pl>
2024-04-23 14:46:37 +0200

    Inital commit

---
+++ APBD7/APBD7/Controllers/WarehouseController.cs
@@ -0,0 +1,43 @@
+﻿using APBD7.Models.DTO_s;
+namespace APBD7.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+
+
+[ApiController]
+[Route("api/animals")]
+
+public class AnimalsController : ControllerBase
+{
+
+    private readonly IConfiguration _configuration;
+
+    public AnimalsController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+    
+
+    [HttpPost]
+    public IActionResult AddAnimal(DTOWarehouseQuerry warehouseQuerry)
+    {
+        using SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("Default"));
+        connection.Open();
+
+        using SqlCommand command = new SqlCommand();
+        command.Connection = connection;
+        command.CommandText = "SELECT IdProduct FROM PRODUCT WHERE IdProduct = @IdProduct";
+        command.Parameters.AddWithValue("@IdProduct", warehouseQuerry.IdProduct);
+        
+        var reader = command.ExecuteReader();
+        
+        while (reader.Read())
+        {
+            int id = reader.GetInt32(reader.GetOrdinal("IdProduct"));
+        }
+        
+        return Created();
+    }
+}
---
+++ APBD7/APBD7/Models/DTO's/DTOWarehouseQuerry.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APBD7.Models.DTO_s;
+
+public class DTOWarehouseQuerry
+{
+    [Required]
+    public int IdProduct { get; set; }
+    [Required]
+    public int IdWarehouse { get; set; }
+    [Required]
+    public int Amount { get; set; }
+    [Required]
+    public string CreatedAt { get; set; }
+}
